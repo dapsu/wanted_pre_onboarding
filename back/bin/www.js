916a6ce43f96b4ced8ba98a5ca6e8c2a69b2810b
username <greenrock46@gmail.com>
2022-06-13 16:59:13 +0900

    Upgrade: 시퀄라이즈 DB 연동 코드 위치 수정 * index.js: 파일 내 있던 시퀄라이즈 연동 코드 제거 * bin/www/js: 시퀄라이즈 연동 코드 추가 수정 이유: 테스트 시 불필요한 DB연동 로그 뜨지 않게 하기 위해서

--- back/bin/www.js
+++ back/bin/www.js
@@ -1,5 +1,13 @@
 const app = require('../index');
+const { sequelize } =  require('../models');
 
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!');
-});
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log('DB 연결 성공!')
+        app.listen(3000, function() {
+            console.log('Example app listening on port 3000!');
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
--- back/index.js
+++ back/index.js
@@ -1,15 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const { sequelize } = require('./models');
-
-sequelize.sync({ force: false })
-    .then(() => {
-        console.log('DB 연결 성공!')
-    })
-    .catch((err) => {
-        console.error(err);
-    });
 
 // 미들웨어
 if (process.env.NODE_ENV !== 'test') {
